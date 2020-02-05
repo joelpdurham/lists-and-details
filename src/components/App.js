6d2d3f9b8431e22177607952ef9d75177e9fb939
joelpdurham <joeldur@gmail.com>
2020-02-05 13:37:46 -0800

    header renders!

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  // Route,
+  // Switch,
+  // Link
+} from 'react-router-dom';
+
+
+const Header = () => (
+  <>
+    <h1>Studio Ghibli API</h1>
+    <h5>Cause it cute af</h5>
+  </>
+);
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Body />
+    </Router>
+  );
 }
-  
+
