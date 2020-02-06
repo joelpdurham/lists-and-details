927343eebfbd5878ca7de75d0c4955b49f3bf7df
joelpdurham <joeldur@gmail.com>
2020-02-05 17:05:45 -0800

    updated snapshot removed console logs

--- src/services/getFilms.js
+++ src/services/getFilms.js
@@ -1,11 +1,9 @@
 export const getFilms = () => {
-  console.log('MAKING A FETCH CALL');
   return fetch('https://ghibliapi.herokuapp.com/films')
     .then(res => res.json());
 };
 
 export const getFilmsById = (id) => {
-  console.log('MAKING A FETCH CALL');
   return fetch(`https://ghibliapi.herokuapp.com/films/${id}`)
     .then(res => res.json());
 };
